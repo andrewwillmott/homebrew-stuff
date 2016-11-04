e1bd44f3b094be6c377fbd40a41a0f328848f4ea
Andrew Willmott <andrewwillmott@users.noreply.github.com>
2016-11-04 22:10:30 +0000

    Add hash

--- ev.rb
+++ ev.rb
@@ -1,8 +1,9 @@
-class EV < Formula
+class Ev < Formula
   desc "Expression evaluator"
   homepage "https://github.com/andrewwillmott/ev"
   url "https://github.com/andrewwillmott/ev/archive/feb89e959cdfc6ba5aa2bdd7d36395a20cbba416.tar.gz"
   version "1"
+  sha256 "576ce34d42a500ebd65263ec91fc739a999111ea85b5036ae7fc77d48da4c281"
 
   def install
     system "c++", "CLExpr.cpp", "EV.cpp", "-o", "ev"
