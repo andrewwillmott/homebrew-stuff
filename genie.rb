fca47d1fefb0b286a1df277a85af3ce339c95081
Andrew Willmott <andrewwillmott@users.noreply.github.com>
2016-11-04 19:55:16 +0000

    Update to latest

--- genie.rb
+++ genie.rb
@@ -1,8 +1,8 @@
 class Genie < Formula
   desc "GENie, "
   homepage "https://github.com/bkaradzic/genie"
-  url "https://github.com/bkaradzic/genie/archive/f8afcab032568126ed80d9a44cc2b64704f2a2fb.tar.gz"
-  version "688"
+  url "https://github.com/bkaradzic/genie/archive/97d06541aef5b798b76fbd19230c8c64f05aa187.tar.gz"
+  version "703"
   sha256 "1c38a9399d16417358846a54742d2f39a3f2d5616435cfbe2a6d2e4862159b2d"
 
   def install
