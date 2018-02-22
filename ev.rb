d90831f29b8cf4c414877e09ecef9027dc4e57d8
Andrew Willmott <andrewwillmott@users.noreply.github.com>
2018-02-22 02:56:59 +0000

    Update sha

--- ev.rb
+++ ev.rb
@@ -4,7 +4,7 @@ class Ev < Formula
   url "https://github.com/andrewwillmott/ev/archive/dbe8d439bbcc6953407b10e2f2dfe9d76af96506.tar.gz"
   version "2"
   sha256 "576ce34d42a500ebd65263ec91fc739a999111ea85b5036ae7fc77d48da4c281"
-
+ 
   def install
     system "c++", "CLExpr.cpp", "EV.cpp", "-o", "ev"
     bin.install("ev")
