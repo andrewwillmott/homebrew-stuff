0e0f82746f56f1341bd9e96c38741227d350421a
Andrew Willmott <awillmott+git@gmail.com>
2019-01-17 17:45:13 +0000

    update checksum

--- distocc.rb
+++ distocc.rb
@@ -3,7 +3,7 @@ class Distocc < Formula
   homepage "https://github.com/andrewwillmott/distance-occlusion"
   url "https://github.com/andrewwillmott/distance-occlusion/archive/master.tar.gz"
   version "2"
-  sha256 "491456470cc22f439704e278bf024f3e3f9fbaa3809548e1a0c949295401904a"
+  sha256 "8532ae01fa82e6fa3b2b145f77b92dda616fcf033a6ea9f781d65fc7dd3c8ea5"
 
   def install
     system "c++", "-std=c++11", "-O3", "-DNDEBUG", "DistanceOcclusionLib.cpp", "DistanceOcclusionTool.cpp", "-o", "distocc"
