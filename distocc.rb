5946d47b87163f5b52ac678d2102d13da5119e78
Andrew Willmott <awillmott+git@gmail.com>
2019-01-17 17:23:18 +0000

    updates for latest release

--- distocc.rb
+++ distocc.rb
@@ -1,17 +1,17 @@
-class Distocc < Formula
-  desc "Tool for generating (signed) distance and approximate AO fields"
+class DistOcc < Formula
+  desc "Tool for generating distance and approximate AO"
   homepage "https://github.com/andrewwillmott/distance-occlusion"
   url "https://github.com/andrewwillmott/distance-occlusion/archive/master.tar.gz"
-  version "2"
-  sha256 "8532ae01fa82e6fa3b2b145f77b92dda616fcf033a6ea9f781d65fc7dd3c8ea5"
+  version "1"
+  sha256 "939985926abd6b70eeacbd414912b8ae5631eb30a12f473f86e725bf76d85ac1"
 
   def install
-    system "c++", "-std=c++11", "-O3", "-DNDEBUG", "DistanceOcclusionLib.cpp", "DistanceOcclusionTool.cpp", "-o", "distocc"
-    bin.install("distocc")
+    system "c++", "-D", "USE_TGA=1", "-D", "USE_OBJ=1", "DistanceOcclusionLib.cpp", "DistanceOcclusionTest.cpp", "MeshSupport.cpp", "targa.c", "-o", "dist-occ"
+    bin.install("dist-occ")
   end
 
   test do
-    system "distocc", "-s"
-    system "open", "signed-distances.png"
+    system "dist-occ", "-d", "-x"
+    system "open", "dist.tga"
   end
 end
