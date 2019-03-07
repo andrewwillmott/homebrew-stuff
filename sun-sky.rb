54f20a38902c21b0e48672db7f5d8a0abbbd7bba
Andrew Willmott <awillmott+git@gmail.com>
2019-03-07 23:43:48 +0000

    Updates for latest version

--- sun-sky.rb
+++ sun-sky.rb
@@ -2,15 +2,15 @@ class SunSky < Formula
   desc "Tool for creating sky environments"
   homepage "https://github.com/andrewwillmott/sun-sky"
   url "https://github.com/andrewwillmott/sun-sky/archive/master.tar.gz"
-  version "1"
-  sha256 "2465796ad7fbd457d917eea5c887c4272bb67326a4d01180a74836dfd4977f5c"
+  version "2"
+  sha256 "25961b3b090d7ed9d34498a29ccb6e9896f60adc9954a7b6391a8862acecefbe"
 
   def install
-    system "c++", "SunSky.cpp", "SunSkyTest.cpp", "-o", "sun-sky" # if this fails, try separate make/make install steps
-    bin.install("sun-sky")
+    system "c++", "--std=c++11", "SunSky.cpp", "SunSkyTool.cpp", "-o", "sunsky" # if this fails, try separate make/make install steps
+    bin.install("sunsky")
   end
 
   test do
-    system "sun-sky", "-t", "12", "-s", "clearUtahBRDF", "-r", "0.3"
+    system "sun-sky", "-t", "12", "-s", "hosekBRDF", "-r", "0.3"
   end
 end
