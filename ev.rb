5f85b79cecace9867e2bffaf2cc4af1cca935379
Andrew Willmott <awillmott+git@gmail.com>
2019-03-11 13:35:57 +0000

    Update for latest rev

--- ev.rb
+++ ev.rb
@@ -1,12 +1,12 @@
 class Ev < Formula
   desc "Expression evaluator"
   homepage "https://github.com/andrewwillmott/ev"
-  url "https://github.com/andrewwillmott/ev/archive/dbe8d439bbcc6953407b10e2f2dfe9d76af96506.tar.gz"
-  version "2"
-  sha256 "0bcb1fe518affe50b96cf59b2c607294110c247d925d9191bc45764a918a5072"
+  url "https://github.com/andrewwillmott/ev/archive/849426d278af42043b41ca1f6ccef427c3195ea7.tar.gz"
+  version "3"
+  sha256 "0be5e84a04658ba4e91444cb231242bca7ad801769290a1d3366d494661e0378"
  
   def install
-    system "c++", "CLExpr.cpp", "EV.cpp", "-o", "ev"
+    system "c++", "-O3", "CLExpr.cpp", "EV.cpp", "-o", "ev"
     bin.install("ev")
   end
 
