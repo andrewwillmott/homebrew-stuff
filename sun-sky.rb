93ef2ca6b5139fff3aa737cc5ff9abaddf325bb5
Andrew Willmott <awillmott+git@gmail.com>
2020-04-12 01:03:40 +0100

    update hash

--- sun-sky.rb
+++ sun-sky.rb
@@ -2,8 +2,8 @@ class SunSky < Formula
   desc "Tool for creating sky environments"
   homepage "https://github.com/andrewwillmott/sun-sky"
   url "https://github.com/andrewwillmott/sun-sky/archive/master.tar.gz"
-  version "2"
-  sha256 "25961b3b090d7ed9d34498a29ccb6e9896f60adc9954a7b6391a8862acecefbe"
+  version "3"
+  sha256 "5bdb404bafe35f3ce1d554fd95e79536daa4a7d7bb1553b13505954f8ce091ed"
 
   def install
     system "c++", "--std=c++11", "SunSky.cpp", "SunSkyTool.cpp", "-o", "sunsky" # if this fails, try separate make/make install steps
