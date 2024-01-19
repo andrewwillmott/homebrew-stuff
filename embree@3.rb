b1d81e52fd495afdadac02595295ffa01a05ca7b
Andrew Willmott <andrew@odyssey.systems>
2024-01-19 17:23:47 +0000

    buildable embree@3 for AS macs

---
+++ embree@3.rb
@@ -0,0 +1,48 @@
+class EmbreeAT3 < Formula
+  desc "High-performance ray tracing kernels"
+  homepage "https://embree.github.io/"
+  url "https://github.com/embree/embree/archive/refs/tags/v3.13.5.tar.gz"
+  sha256 "b8c22d275d9128741265537c559d0ea73074adbf2f2b66b0a766ca52c52d665b"
+  license "Apache-2.0"
+  head "https://github.com/embree/embree.git", branch: "v3.13.5"
+
+  depends_on "cmake" => :build
+  depends_on "ispc" => :build
+  depends_on "tbb"
+
+  def install
+    args = std_cmake_args + %w[
+      -DBUILD_TESTING=OFF
+      -DEMBREE_IGNORE_CMAKE_CXX_FLAGS=OFF
+      -DEMBREE_ISPC_SUPPORT=ON
+      -DEMBREE_TUTORIALS=OFF
+    ]
+    args << "-DEMBREE_MAX_ISA=#{MacOS.version.requires_sse42? ? "SSE4.2" : "SSE2"}" if Hardware::CPU.intel?
+
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "make"
+      system "make", "install"
+    end
+
+    # Remove bin/models directory and the resultant empty bin directory since
+    # tutorials are not enabled.
+    rm_rf bin
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <assert.h>
+      #include <embree3/rtcore.h>
+      int main() {
+        RTCDevice device = rtcNewDevice("verbose=1");
+        assert(device != 0);
+        rtcReleaseDevice(device);
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lembree3"
+    system "./a.out"
+  end
+end
